1cb978667e4411cb8129a696f7d826dd5e7d8aa7
Sharath <Sharus9587@gmail.com>
2021-09-18 21:26:40 +0530

    Added : cleared the error while redirecting from the mail

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -3,41 +3,39 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { UserService } from 'src/app/Services/user.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { MatSnackBarConfig } from '@angular/material/snack-bar';
-
-
-
+import {MatSnackBarConfig} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.scss']
 })
-export class ResetPasswordComponent implements OnInit
-{
+export class ResetPasswordComponent implements OnInit {
   resetpassword!: FormGroup;
   submitted = false;
   token: any;
+ 
 
-  constructor(private formBuilder: FormBuilder, public snackBar: MatSnackBar, private activeRoute: ActivatedRoute, private user: UserService) { }
-
-  openSnackBar(message: string, duration: number) {
-    let config = new MatSnackBarConfig();
-    if (duration != 0) {
-      config.duration = duration;
+  constructor(private formBuilder: FormBuilder,
+    public snackBar: MatSnackBar,  private activeRoute: ActivatedRoute,private user: UserService) { }
+  
+    openSnackBar(message: string, duration: number) {
+      let config = new MatSnackBarConfig();
+      if (duration != 0) {
+        config.duration = duration;
+      }
+      this.snackBar.open(message, undefined, config);
     }
-    this.snackBar.open(message, undefined, config);
-  }
-
 
   ngOnInit(): void {
     this.resetpassword = this.formBuilder.group({
 
       password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', Validators.required]
+      confirmPassword: ['', Validators.required],
     });
     this.token = this.activeRoute.snapshot.paramMap.get('token');
   }
+
   get f() { return this.resetpassword.controls; }
 
   onSubmit() {
@@ -47,24 +45,21 @@ export class ResetPasswordComponent implements OnInit
         password:this.resetpassword.value.password,
         confirmPassword:this.resetpassword.value.confirmPassword
       }
-      console.log(reqData);
-      
       this.user.resetpassword(this.token,reqData).subscribe(
         (response : any) => {
-          this.openSnackBar('Password Reset Successfully', 2000);
+          this.openSnackBar('Password Reset Successfull', 3000);
         },
         error => {
           if (error['status'] == 0) {
-            this.openSnackBar('Password Reset Failed: Server Offline', 2000,);
+            this.openSnackBar('Password Reset Failed: Server Offline', 3000,);
           }
           else {
-            this.openSnackBar('Password Reset Failed:', 2000);
+            this.openSnackBar('Password Reset Failed:', 3000);
           }
         });
     }
   }
   reqData(reqData: any) {
-    throw new Error('Method not implemented.');  
-  
-  } 
- }
+    throw new Error('Method not implemented.');
+  }
+}
