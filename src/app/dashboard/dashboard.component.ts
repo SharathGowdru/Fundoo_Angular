47eb669586d54f5c17001a987f5c01cb72628f1d
Sharath <Sharus9587@gmail.com>
2021-09-18 06:12:53 +0530

    Added : side navigation and logout button

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,9 +11,21 @@ export class DashboardComponent implements OnInit {
 
   constructor( private router: Router ) { }
 
+   fontcolors = ['color : #4285F4', 
+                'color : #EA4335', 
+                'color : #FBBC05', 
+                'color : #4285F4', 
+                'color : #EA4335',
+                'color : #FBBC05']
+  fontletters = ['F', 'u', 'n', 'D', 'o', 'o']
+
   ngOnInit(): void {
   }
   toggleSidebar(){
     this.opened = !this.opened
+  }
+  logout(){
+    localStorage.clear();
+    this.router.navigateByUrl('/login')
   }
 }
