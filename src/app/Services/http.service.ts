1a1f92b6d9de88b609fc1000cb3ce17bf4678576
Sharath <Sharus9587@gmail.com>
2021-09-29 09:54:21 +0530

    Added : Post and Get service

--- src/app/Services/http.service.ts
+++ src/app/Services/http.service.ts
@@ -1,25 +1,31 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+// import { url } from 'inspector';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-  BaseUrl=environment.BaseUrl;
+
+  baseUrl = environment.baseUrl;
   token : any;
+  httpClient: any;
 
   constructor(private http: HttpClient) { }
 
-  Post(url: any, data: any, token: any, headers: boolean){
-    //  this.token = localStorage.getItem('Token');
-    let options = {
-      headers: new HttpHeaders({
-        // 'Authorization': this.token,
-        'Content-Type': 'application/json'
-      })
-    }
-
-    return this.http.post(this.BaseUrl + url, data,options);
+  postService( url: string = '',payload: any = null, tokenRequired: boolean = false, httpOptions:any=null) {
+    // console.log(url);
+    // console.log(tokenRequired);
+    return this.http.post(this.baseUrl+url, payload, tokenRequired && httpOptions);
   }
+  getService( url: string = '' , tokenRequired: boolean = false, httpOptions:any=null) {
+    // console.log(url);
+    // console.log(tokenRequired);
+    return this.http.get(this.baseUrl+url, tokenRequired && httpOptions);
+  }
+  // putService( url: string, payload: any,httpOptions:any=null){
+
+  //   return this.http.put(this.baseUrl+url, payload, httpOptions)
+  // }
 }
